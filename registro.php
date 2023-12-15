5c43e2a5cce371d83b0ffe102e9209977a0f9b4b
Karlosbrito <Karlosbrito0@gmail.com>
2023-12-15 00:58:20 -0400

    Cambio a al css y a los campos

--- registro.php
+++ registro.php
@@ -1,12 +1,13 @@
 <?php ob_start(); ?>
 <!DOCTYPE html>
+
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Registrar servidor</title>
-  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open Sans:400,300,700' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="style_registro.css">
 </head>
 
@@ -15,68 +16,109 @@
     <div class="frame">
       <div class="nav">
         <ul class="links">
-          <li class="signin-active"><a class="btn">Registro</a></li>
+          <li class="signup-active"><a class="btn">Registro</a></li>
         </ul>
       </div>
-      <div ng-app ng-init="checked = false">
-        <form class="form-signup" action="conexiones\registro_servidores.php" method="post" name="form">
-
-          <label for="id">ID del servidor</label>
-          <input class="form-styling" type="number" name="id" placeholder="">
-
-          <label for="nombre">Nombre del servidor</label>
-          <input class="form-styling" type="text" name="nombre" placeholder="">
-
-          <label for="ip">IP del servidor</label>
-          <input class="form-styling" type="text" name="ip" placeholder="">
-
-          <label for="tipos">Tipo</label>
-          <input class="form-styling" type="text" name="tipos" placeholder="">
-
-          <label for="ubicacion">Ubicación</label>
-          <input class="form-styling" type="text" name="ubicacion" placeholder="">
-
-          <label for="so">SO</label>
-          <input class="form-styling" type="text" name="so" placeholder="">
-
-          <label for="servicios">Servicios</label>
-          <input class="form-styling" type="text" name="servicios" placeholder="">
-
-          <label for="caracteristicas">Características</label>
-          <input class="form-styling" type="text" name="caracteristicas" placeholder="">
-
-          <label for="tipo_plataforma">Tipo de plataforma</label>
-          <input class="form-styling" type="text" name="tipo_plataforma" placeholder="">
-
-          <label for="observaciones">Observaciones</label>
-          <input class="form-styling" type="text" name="observaciones" placeholder="">
-
-          <label for="dependencias">Dependencias</label>
-          <input class="form-styling" type="text" name="dependencias" placeholder="">
-
-          <label for="conexiones">Conexiones</label>
-          <input class="form-styling" type="text" name="conexiones" placeholder="">
-
-          <label for="tipo_red">Tipo de red</label>
-          <input class="form-styling" type="text" name="tipo_red" placeholder="">
-
-          <label for="estatus">Estatus</label>
-          <input class="form-styling" type="text" name="estatus" placeholder="">
-
-          <input type="submit" value="Registrar servidor" class="btn-signup" id="submitBtn">
-
-        </form>
-        <div class="message-container">
-          <p class="registration-message">Servidor registrado</p>
-          <a href="logo.html" class="btn-goback">Volver al inicio</a>
+      <div class="form-signup">
+      <form action="conexiones\registro_servidores.php" method="post">
+        <div class="row">
+          <div class="col-6">
+            <label for="id" class="text-left">ID del servidor</label>
+            <input class="form-styling" type="number" name="id" placeholder="">
+          </div>
+          <div class="col-6">
+            <label for="nombre" class="text-left">Nombre del servidor</label>
+            <input class="form-styling" type="text" name="nombre" placeholder="">
+          </div>
         </div>
+
+        <div class="row">
+          <div class="col-6">
+            <label for="ip" class="text-left">IP del servidor</label>
+            <input class="form-styling" type="text" name="ip" placeholder="">
+          </div>
+          <div class="col-6">
+            <label for="tipos" class="text-left">Tipo</label>
+            <input class="form-styling" type="text" name="tipos" placeholder="">
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-6">
+            <label for="ubicacion" class="text-left">Ubicación</label>
+            <input class="form-styling" type="text" name="ubicacion" placeholder="">
+          </div>
+          <div class="col-6">
+            <label for="so" class="text-left">SO</label>
+            <input class="form-styling" type="text" name="so" placeholder="">
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-6">
+            <label for="servicios" class="text-left">Servicios</label>
+            <input class="form-styling" type="text" name="servicios" placeholder="">
+          </div>
+          <div class="col-6">
+            <label for="caracteristicas" class="text-left">Características</label>
+            <input class="form-styling" type="text" name="caracteristicas" placeholder="">
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-6">
+            <label for="tipo_plataforma" class="text-left">Tipo de plataforma</label>
+            <input class="form-styling" type="text" name="tipo_plataforma" placeholder="">
+          </div>
+          <div class="col-6">
+            <label for="observaciones" class="text-left">Observaciones</label>
+            <input class="form-styling" type="text" name="observaciones" placeholder="">
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-6">
+            <label for="dependencias" class="text-left">Dependencias</label>
+            <input class="form-styling" type="text" name="dependencias" placeholder="">
+          </div>
+          <div class="col-6">
+            <label for="conexiones" class="text-left">Conexiones</label>
+            <input class="form-styling" type="text" name="conexiones" placeholder="">
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-6">
+            <label for="dependencias" class="text-left">Dependencias</label>
+            <input class="form-styling" type="text" name="dependencias" placeholder="">
+          </div>
+          <div class="col-6">
+            <label for="conexiones" class="text-left">Conexiones</label>
+            <input class="form-styling" type="text" name="conexiones" placeholder="">
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-6">
+            <label for="tipo_red" class="text-left">Tipo de Red</label>
+            <input class="form-styling" type="text" name="tipo_red" placeholder="">
+          </div>
+          <div class="col-6">
+            <label for="estatus" class="text-left">Estatus</label>
+            <input class="form-styling" type="text" name="estatus" placeholder="">
+          </div>
+        </div>
+
+        <input type="submit" value="Registrar servidor" class="btn-signup" id="submitBtn">
       </div>
+    
     </div>
   </div>
 
 </body>
 
 </html>
+
 <?php $contents = ob_get_clean(); ?>
 <?php 
 
