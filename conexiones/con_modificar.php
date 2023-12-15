f858cdf40e83ec4516e60b6270c53d4d5dd7b0f5
Karlosbrito <Karlosbrito0@gmail.com>
2023-12-15 18:15:39 -0400

    Modificar ya hecho con css
    A falta de nombre identificador para los campos

--- conexiones/con_modificar.php
+++ conexiones/con_modificar.php
@@ -1,56 +1,45 @@
 <?php
-  // Obtenemos el ID del servidor a modificar
-  $id = $_GET['id'];
 
-  // Conectamos a la base de datos
-  $conexion = new PDO("pgsql:host=localhost;dbname=Movilnet;user=postgres;password=postgres");
+// Conectar a la base de datos
+$db = new PDO("pgsql:host=localhost;dbname=Movilnet", "postgres", "postgres");
 
-  // Consultamos el servidor
-  $consulta = "SELECT * FROM servidores WHERE id = $id";
-  $resultado = $conexion->query($consulta);
+// Obtener los datos del formulario
+$id = $_POST["id"];
+$nombre = $_POST["nombre"];
+$ip = $_POST["ip"];
+$tipos = $_POST["tipos"];
+$ubicacion = $_POST["ubicacion"];
+$so = $_POST["so"];
+$servicios = $_POST["servicios"];
+$caracteristicas = $_POST["caracteristicas"];
+$tipo_plataforma = $_POST["tipo_plataforma"];
+$observaciones = $_POST["observaciones"];
+$dependencias = $_POST["dependencias"];
+$conexiones = $_POST["conexiones"];
+$tipo_red = $_POST["tipo_red"];
+$estatus = $_POST["estatus"];
 
-  // Si la consulta es exitosa
-  if ($resultado->rowCount() > 0) {
-    // Obtenemos el registro del servidor
-    $registro = $resultado->fetch_assoc();
+// Actualizar el registro en la base de datos
+$sql = "UPDATE servidores SET
+  nombre = '$nombre',
+  ip = '$ip',
+  tipos = '$tipos',
+  ubicacion = '$ubicacion',
+  so = '$so',
+  servicios = '$servicios',
+  caracteristicas = '$caracteristicas',
+  tipo_plataforma = '$tipo_plataforma',
+  observaciones = '$observaciones',
+  dependencias = '$dependencias',
+  conexiones = '$conexiones',
+  tipo_red = '$tipo_red',
+  estatus = '$estatus'
+WHERE id = $id";
 
-    // Rellenamos los campos del formulario con los datos del servidor
-    $nombre_completo = $registro['nombre_completo'];
-    $ip = $registro['ip'];
-    $tipos = $registro['tipos'];
+$stmt = $db->prepare($sql);
+$stmt->execute();
 
-    // Actualizamos los datos del servidor en la base de datos
-    $consulta = "UPDATE servidores
-    SET nombre_completo = '$nombre_completo',
-    ip = '$ip',
-    tipos = '$tipos',
-    ubicacion = '$ubicacion',
-    so = '$so',
-    servicios = '$servicios',
-    caracteristicas = '$caracteristicas',
-    tipo_plataforma = '$tipo_plataforma',
-    observaciones = '$observaciones',
-    dependencias = '$dependencias',
-    conexiones = '$conexiones',
-    tipo_red = '$tipo_red',
-    estatus = '$estatus'
-    WHERE id = $id";
+// Redireccionar a la página principal
+header("Location: consulta_servidores.php");
 
-    $conexion->query($consulta);
-
-    // Si la actualización fue exitosa
-    if ($conexion->rowCount() > 0) {
-      // Redirigimos al usuario a la página de inicio
-      header("Location: index.php");
-    }
-  }
-
-  // Si la consulta no fue exitosa
-  else {
-    // Mostramos un mensaje de error
-    echo "No se encontró el servidor.";
-  }
-
-  // Cerramos la conexión con la base de datos
-  $conexion = null;
 ?>
