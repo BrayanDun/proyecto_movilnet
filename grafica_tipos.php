1f5c96d596c790222406c34adfac7b7a36a8d114
GpsRuiz <gaboruizluques@gmail.com>
2024-02-02 02:28:45 -0400

    graficas
    hay un error con la muestra de informacion, pero esta listo el molde de las graficas

---
+++ grafica_tipos.php
@@ -0,0 +1,54 @@
+<?php ob_start(); ?>
+<?php 
+$host = "localhost";
+$dbname = "Movilnet";
+$username = "postgres";
+$password = "postgres";
+
+// Crea una conexión a la base de datos
+$conn = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
+
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load("current", {packages:["corechart"]});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+////aqui esta el error//////
+<?php
+      $SQL_READ = "SELECT tipos FROM servidores";
+      $stmt = $conn->prepare($SQL_READ);
+      while ($stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo "['".$stmt[':tipos']."']";
+      }
+?>
+////aqui esta el error//////no se esta mostrando la info de la bd
+        ]);
+
+        var options = {
+          title: 'Grafica de tipos',
+          is3D: true,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="piechart_3d" style="width: 900px; height: 500px;"></div>
+  </body>
+</html>
+
+<?php $contents = ob_get_clean(); ?>
+
+<?php 
+
+require("./index_usuario.php");
+
+?>
